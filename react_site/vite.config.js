f0f0d26ab9feb8b106ecb477a07d7faada432efc
kacety123 <krystal.thode@toadworld.org>
2024-09-12 12:20:40 -0400

    updating vite config

--- react_site/vite.config.js
+++ react_site/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'https://kacety123.github.io/react_site'
+    base: 'https://kacety123.github.io/react_site/react_site'
 })
